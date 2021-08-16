5998f2848f24d3d7c83472e70f938642050efd2b
Tom Houlé <tom@tomhoule.com>
2021-08-16 20:54:32 +0200

    Emacs config updates
    - Enable nix mode
    - Stop the quatsch about semi-light mono font

--- doom/config.el
+++ doom/config.el
@@ -21,7 +21,7 @@
 ;; font string. You generally only need these two:
 ;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
 ;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
-(setq doom-font (font-spec :family "monospace" :size 22 :weight 'semi-light)
+(setq doom-font (font-spec :family "monospace" :size 22)
       doom-variable-pitch-font (font-spec :family "sans" :size 22))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
