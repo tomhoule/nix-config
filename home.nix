a04c4c0bcc34afa442ab33502ebd53a445119895
Tom Houlé <tom@tomhoule.com>
2021-09-22 08:18:22 +0200

    Set up overlay patch for flake crashes
    See the group of issues following links from https://github.com/NixOS/nix/issues/3271

--- home.nix
+++ home.nix
@@ -23,7 +23,7 @@ in
   # You can update Home Manager without changing this value. See
   # the Home Manager release notes for a list of state version
   # changes in each release.
-  home.stateVersion = "21.11";
+  home.stateVersion = "21.05";
 
   home.packages = [
     # Coq & OCaml
--- overlays.nix
+++ overlays.nix
@@ -1,5 +1,11 @@
 [
-    (import (builtins.fetchTarball {
-      url = https://github.com/nix-community/emacs-overlay/archive/master.tar.gz;
-    }))
+  (import (builtins.fetchTarball {
+    url =
+      "https://github.com/nix-community/emacs-overlay/archive/master.tar.gz";
+  }))
+
+  (self: super: {
+    nixUnstable =
+      super.nixUnstable.override { patches = [ ./unset-is-macho.patch ]; };
+  })
 ]
