9bf663e0478a0ea77fa269fe975bbfc2f103cc6a
Tom Houlé <tom@tomhoule.com>
2021-10-02 12:50:35 +0200

    First attempt at a NixOS config

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "noch nix";
+
+  inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
+  inputs.home-manager.url = "github:nix-community/home-manager";
+  outputs = { self, nixpkgs, home-manager }:
+    let system = "x86_64-linux";
+    in {
+      nixosConfigurations = {
+        test-vm = nixpkgs.lib.nixosSystem {
+          modules = [
+            ./test-vm/config.nix
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.tom = ./home.nix;
+            }
+          ];
+          inherit system;
+        };
+      };
+    };
+}
