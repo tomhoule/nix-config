09bbd03bde6616cd9185da173c860a566ba2d7e0
Tom Houlé <tom@tomhoule.com>
2021-10-02 17:09:02 +0200

    Move nixos base config to a new ./modules folder

--- flake.nix
+++ flake.nix
@@ -1,14 +1,18 @@
 {
   description = "noch nix";
 
-  inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
-  inputs.home-manager.url = "github:nix-community/home-manager";
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+  };
+
   outputs = { self, nixpkgs, home-manager }:
     let system = "x86_64-linux";
     in {
       nixosConfigurations = {
         test-vm = nixpkgs.lib.nixosSystem {
           modules = [
+            ./modules/base.nix
             ./test-vm/config.nix
             home-manager.nixosModules.home-manager
             {
@@ -16,43 +20,6 @@
               home-manager.useUserPackages = true;
               home-manager.users.tom = ./home.nix;
             }
-            ({ pkgs, ... }: {
-              environment.systemPackages = with pkgs; [
-                firefox
-                foot
-                ripgrep
-                vim
-                zsh
-              ];
-              nix = {
-                package = pkgs.nixUnstable;
-                extraOptions = ''
-                  experimental-features = nix-command flakes
-                '';
-              };
-
-              # Set your time zone.
-              time.timeZone = "Europe/Berlin";
-
-              # Select internationalisation properties.
-              i18n.defaultLocale = "en_US.UTF-8";
-              console = {
-                #   font = "Lat2-Terminus16";
-                keyMap = "fr-bepo";
-              };
-
-              programs = {
-                neovim = {
-                  enable = true;
-                  defaultEditor = true;
-                };
-              };
-              users.users.tom = {
-                isNormalUser = true;
-                extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-                shell = "${pkgs.zsh}/bin/zsh";
-              };
-            })
           ];
           inherit system;
         };
---
+++ modules/base.nix
@@ -0,0 +1,38 @@
+{ config, lib, pkgs, ... }:
+
+{
+  # System packages.
+  environment.systemPackages = with pkgs; [ firefox foot ripgrep vim zsh ];
+
+  nix = {
+    package = pkgs.nixUnstable;
+    extraOptions = ''
+      experimental-features = nix-command flakes
+    '';
+  };
+
+  # Set your time zone.
+  time.timeZone = "Europe/Berlin";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  console = {
+    #   font = "Lat2-Terminus16";
+    keyMap = "fr-bepo";
+  };
+
+  programs = {
+    neovim = {
+      enable = true;
+      defaultEditor = true;
+    };
+  };
+
+  users.users.tom = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+    shell = "${pkgs.zsh}/bin/zsh";
+  };
+
+}
