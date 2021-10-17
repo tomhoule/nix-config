b1cf96dc3807acd59a2b9bfd21b7e872f7e8e715
Tom Houlé <tom@tomhoule.com>
2021-10-17 08:39:32 +0200

    Manage default browser with home-manager

--- home.nix
+++ home.nix
@@ -157,13 +157,25 @@ in {
 
     mimeApps = {
       enable = true;
-      defaultApplications = {
+      defaultApplications = let defaultBrowser = "firefox.desktop";
+      in {
         "application/pdf" = [ "org.pwmt.zathura.desktop" ];
         "image/gif" = [ "imv.desktop" ];
         "image/jpeg" = [ "imv.desktop" ];
         "image/png" = [ "imv.desktop" ];
         "image/webp" = [ "imv.desktop" ];
         "video/webm" = [ "mpv.desktop" ];
+
+        "x-scheme-handler/http" = [ defaultBrowser ];
+        "x-scheme-handler/https" = [ defaultBrowser ];
+        "x-scheme-handler/chrome" = [ defaultBrowser ];
+        "text/html" = [ defaultBrowser ];
+        "application/x-extension-htm" = [ defaultBrowser ];
+        "application/x-extension-html" = [ defaultBrowser ];
+        "application/x-extension-shtml" = [ defaultBrowser ];
+        "application/xhtml+xml" = [ defaultBrowser ];
+        "application/x-extension-xhtml" = [ defaultBrowser ];
+        "application/x-extension-xht" = [ defaultBrowser ];
       };
     };
 
