9b603e5abb0cdf8ad1236d5420e31a2902cf89c3
Tom Houlé <tom@tomhoule.com>
2021-10-19 20:30:45 +0200

    Set terminal letter spacing

--- home.nix
+++ home.nix
@@ -87,6 +87,7 @@ in
     settings = {
       main = {
         font = "IBM Plex Mono:size=${localHome.termFontSize or "7"}";
+        letter-spacing = "-0.3";
       };
       colors = {
         alpha = "0.9";
