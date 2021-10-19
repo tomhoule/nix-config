4a3645ab730403b758f22cf2ca3c58fb8c48e665
Tom Houlé <tom@tomhoule.com>
2021-10-19 18:46:10 +0200

    Make terminal font size overridable

--- home.nix
+++ home.nix
@@ -85,7 +85,7 @@ in
     server = { enable = true; };
     settings = {
       main = {
-        font = "IBM Plex Mono:size=12";
+        font = "IBM Plex Mono:size=${localHome.termFontSize or "7"}";
       };
       colors = {
         alpha = "0.9";
