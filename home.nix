b7f8e96f791f04801b55364e6724198fc2c8acd7
Tom Houlé <tom@tomhoule.com>
2021-10-29 18:52:58 +0200

    Split off neovim config

--- home.nix
+++ home.nix
@@ -101,49 +101,7 @@ in
   # Notification daemon
   programs.mako.enable = true;
 
-  programs.neovim = {
-    enable = true;
-    plugins = with pkgs.vimPlugins; [
-      {
-        plugin = dracula-vim;
-        config = ''
-          set termguicolors
-          colorscheme dracula
-          let g:dracula_colorterm = 0
-        '';
-      }
-      {
-        plugin = fzf-vim;
-      }
-      { plugin = fzf-lsp-nvim; }
-      {
-        plugin = nvim-lspconfig;
-        config = "lua << EOF\n${readFile ./dotfiles/nvim-lsp-config.lua}\nEOF";
-      }
-      {
-        plugin = vim-commentary;
-      }
-      {
-        plugin = vim-dirvish;
-        config = ''
-          " Replace netrw with dirvish
-          let g:loaded_netrwPlugin = 1
-          command! -nargs=? -complete=dir Explore Dirvish <args>
-          command! -nargs=? -complete=dir Sexplore belowright split | silent Dirvish <args>
-          command! -nargs=? -complete=dir Vexplore leftabove vsplit | silent Dirvish <args>
-        '';
-      }
-      { plugin = vim-gitgutter; }
-      {
-        plugin = vim-nix;
-        config = ''
-          autocmd BufRead,BufNewFile *.nix nmap <buffer> <leader>f :w<ENTER>:!nixpkgs-fmt %<ENTER><ENTER>:e!<ENTER>
-        '';
-      }
-      { plugin = vim-surround; }
-    ];
-    extraConfig = readFile ./dotfiles/init.vim;
-  };
+  programs.neovim = import ./home/nvim { inherit pkgs; };
 
   programs.git = {
     enable = true;
