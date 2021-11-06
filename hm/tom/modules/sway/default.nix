9e39b6059aeef260e0ed00214acbe8d5abc4b325
Tom Houlé <tom@tomhoule.com>
2021-11-06 10:10:33 +0100

    sway: Make gaps smaller on laptop

--- hm/tom/modules/sway/default.nix
+++ hm/tom/modules/sway/default.nix
@@ -5,6 +5,14 @@
 
   xdg.configFile = {
     "sway/config".source = ./config;
+    "sway/config.d/01-borders-and-gaps".text = lib.mkDefault ''
+      default_border pixel 6
+      smart_borders on
+      smart_gaps on
+      gaps inner 10
+      gaps outer 0
+    '';
+
     "waybar".source = lib.mkDefault ./waybar;
   };
 }
--- hm/tom/per-machine/xps13.nix
+++ hm/tom/per-machine/xps13.nix
@@ -1,6 +1,15 @@
 { ... }:
 
 {
-  xdg.configFile."waybar".source = ../modules/sway/laptopWaybar;
+  xdg.configFile = {
+    "waybar".source = ../modules/sway/laptopWaybar;
+    "sway/config.d/01-borders-and-gaps".text = ''
+      default_border pixel 6
+      smart_borders on
+      smart_gaps on
+      gaps inner 6
+      gaps outer 0
+    '';
+  };
 }
 
