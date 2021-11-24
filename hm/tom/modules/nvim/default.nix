6be035d6111c2f56072c713cad21e62272d2e501
Tom Houlé <tom@tomhoule.com>
2021-11-24 15:27:12 +0100

    Add treesitter config to nvim

--- hm/tom/modules/nvim/default.nix
+++ hm/tom/modules/nvim/default.nix
@@ -59,6 +59,32 @@ with builtins;
 
 
       {
+        plugin = (nvim-treesitter.withPlugins (plugins: pkgs.tree-sitter.allGrammars));
+        config = ''
+          lua <<EOF
+            require('nvim-treesitter.configs').setup {
+              highlight = {
+                enable = true,              -- false will disable the whole extension
+                -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+                -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+                -- Using this option may slow down your editor, and you may see some duplicate highlights.
+                -- Instead of true it can also be a list of languages
+                additional_vim_regex_highlighting = false,
+              },
+              incremental_selection = {
+                enable = true,
+                keymaps = {
+                  init_selection = '<CR>',
+                  scope_incremental = '<CR>',
+                  node_incremental = '<TAB>',
+                  node_decremental = '<S-TAB>',
+                },
+              },
+            }
+          EOF
+        '';
+      }
+      {
         plugin = nvim-lspconfig;
         config = "lua << EOF\n${readFile ./nvim-lsp-config.lua}\nEOF";
       }
