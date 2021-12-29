6e9f8627fb8ab087eb7d5640a6698b4e8d9cff5d
Tom Houlé <tom@tomhoule.com>
2021-12-29 14:26:47 +0100

    Add lean nvim setup

---
+++ hm/tom/modules/lean.nix
@@ -0,0 +1,23 @@
+{ pkgs, config, ... }:
+
+let
+  nvimConfig = ''
+    lua <<EOF
+      require('lean').setup{
+        abbreviations = { builtin = true },
+        lsp = { on_attach = on_attach },
+        lsp3 = { on_attach = on_attach },
+        mappings = true,
+      }
+    EOF
+  ''; in
+{
+  programs.neovim.plugins = [
+    pkgs.vimPlugins.plenary-nvim
+
+    {
+      plugin = pkgs.vimPlugins.lean-nvim;
+      config = nvimConfig;
+    }
+  ];
+}
