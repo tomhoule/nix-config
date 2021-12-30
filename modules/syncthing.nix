c577f8e30b19339d7cb41d15eeb8a326c2c4112a
Tom Houlé <tom@tomhoule.com>
2021-12-30 10:53:17 +0100

    Add syncthing module

---
+++ modules/syncthing.nix
@@ -0,0 +1,21 @@
+{ config, lib, pkgs, ... }:
+
+let
+  groupName = "syncthing";
+  guiAddress = "127.0.0.1:8384";
+  syncthing-ui = pkgs.writeShellScriptBin "syncthing-ui" ''
+    xdg-open http://${guiAddress}
+  '';
+in
+{
+  environment.systemPackages = [ syncthing-ui ];
+  users.users.tom.extraGroups = [ groupName ];
+
+  services.syncthing = {
+    enable = true;
+    group = groupName;
+    overrideDevices = false;
+    overrideFolders = false;
+    inherit guiAddress;
+  };
+}
