194abce6e6a53f98c5e4f4c1386a2cfbff722e2c
Tom Houlé <tom@tomhoule.com>
2022-03-15 21:50:25 +0100

    Simplify julia module

--- hm/tom/modules/julia.nix
+++ hm/tom/modules/julia.nix
@@ -2,11 +2,9 @@
 
 {
   home.packages = [
-    julia-stable-bin # fancy calculator
+    pkgs.julia-stable-bin # fancy calculator
   ];
 
   home.sessionVariables.JULIA_DEPOT_PATH = "${config.xdg.cacheHome}/julia";
-
-  programs.neovim.plugins = [{ plugin = pkgs.vimPlugins.julia-vim; }];
 }
 
