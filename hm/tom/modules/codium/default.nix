e8cfb853dc35bf2214201e08ebe7602294f01917
Tom Houlé <tom@tomhoule.com>
2022-09-11 19:05:20 +0200

    codium: give up on managing configuration in nix
    The editor is actively working against it.

--- hm/tom/modules/codium/default.nix
+++ hm/tom/modules/codium/default.nix
@@ -4,30 +4,5 @@
   programs.vscode = {
     enable = true;
     package = pkgs.vscodium;
-    extensions = with pkgs.vscode-extensions; [
-      dracula-theme.theme-dracula
-      jnoortheen.nix-ide
-      # matklad.rust-analyzer # does not build at this point in time (2021-11-03)
-      tamasfe.even-better-toml
-    ];
-    userSettings = {
-      editor = {
-        fontFamily = "'IBM Plex Mono', 'Fira Code'";
-        fontSize = 17;
-        formatOnSave = true;
-        minimap.enabled = false;
-      };
-      files = {
-        insertFinalNewline = true;
-        trimFinalNewlines = true;
-        trimTrailingWhitespace = true;
-      };
-      telemetry.telemetryLevel = "off";
-      window = {
-        menuBarVisibility = "hidden";
-        titleBarStyle = "native";
-      };
-      workbench.colorTheme = "Dracula";
-    };
   };
 }
