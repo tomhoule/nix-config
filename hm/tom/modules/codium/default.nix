52e1ce88c35ddd23ad5d6eb162575578452023f6
Tom Houlé <tom@tomhoule.com>
2022-09-24 10:35:26 +0200

    codium: immutable configuration
    If I use it, the config and extensions directory should all be in the
    nixos configuration. This is safer for things like disabling telemetry.

--- hm/tom/modules/codium/default.nix
+++ hm/tom/modules/codium/default.nix
@@ -4,5 +4,26 @@
   programs.vscode = {
     enable = true;
     package = pkgs.vscodium;
+
+    mutableExtensionsDir = false;
+    extensions = with pkgs.vscode-extensions; [
+      bbenoist.nix
+    ];
+
+    keybindings = [ ];
+    userSettings = {
+      editor.fontFamily = "Hack, 'Droid Sans Mono', 'monospace', monospace";
+      editor.fontLigatures = false;
+      editor.fontSize = 12;
+      editor.minimap.enabled = false;
+      files.trimFinalNewlines = false;
+      files.trimTrailingWhitespace = false;
+      telemetry.enableCrashReporter = false;
+      telemetry.enableTelemetry = true;
+      telemetry.telemetryLevel = "off";
+      window.menuBarVisibility = "hidden";
+      window.zoomLevel = 2;
+      workbench.colorTheme = "Quiet Light";
+    };
   };
 }
