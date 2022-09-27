94e66a70d07f6a5b74939ffdae016554dd1c9011
Tom Houlé <tom@tomhoule.com>
2022-09-27 08:56:31 +0200

    laptop/tlp: one more setting

--- modules/laptop.nix
+++ modules/laptop.nix
@@ -16,6 +16,7 @@
       # https://linrunner.de/tlp/settings/battery.html
       START_CHARGE_THRESH_BAT0 = 75;
       STOP_CHARGE_THRESH_BAT0 = 80;
+      RESTORE_THRESHOLDS_ON_BAT=1
 
       CPU_SCALING_GOVERNOR_ON_AC = "performance";
       CPU_SCALING_GOVERNOR_ON_BAT = "schedutil";
