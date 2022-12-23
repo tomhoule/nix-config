567a944ef9973676379a38f19ccd695751c062e7
Tom Houlé <tom@tomhoule.com>
2022-12-23 11:15:36 +0100

    nvim/telescope: remap C-q to opening selection in trouble

--- hm/tom/modules/nvim/telescope.lua
+++ hm/tom/modules/nvim/telescope.lua
@@ -1,20 +1,21 @@
-local trouble = require("trouble")
+local trouble = require("trouble.providers.telescope")
+
 require("telescope").setup({
-defaults = {
-  mappings = {
-    i = { ["<c-t>"] = trouble.open_with_trouble },
-    n = { ["<c-t>"] = trouble.open_with_trouble },
-  },
-},
-extensions = {
-  fzf = {
-    fuzzy = true,                    -- false will only do exact matching
-    override_generic_sorter = true,  -- override the generic sorter
-    override_file_sorter = true,     -- override the file sorter
-    case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
-    -- the default case_mode is "smart_case"
-  }
-}
+    defaults = {
+        mappings = {
+            i = { ["<c-q>"] = trouble.open_with_trouble },
+            n = { ["<c-q>"] = trouble.open_with_trouble },
+        },
+    },
+    extensions = {
+        fzf = {
+            fuzzy = true,                    -- false will only do exact matching
+            override_generic_sorter = true,  -- override the generic sorter
+            override_file_sorter = true,     -- override the file sorter
+            case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+            -- the default case_mode is "smart_case"
+        }
+    }
 })
 
 -- To get fzf loaded and working with telescope, you need to call
