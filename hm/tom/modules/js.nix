ef3f5d7c879e27eaf1aece83b2c019006574c8f3
Tom Houlé <tom@tomhoule.com>
2023-10-24 06:22:50 +0200

    hm: more js dev packages

--- hm/tom/modules/js.nix
+++ hm/tom/modules/js.nix
@@ -4,6 +4,9 @@
   home.packages = with pkgs; [
     bun
     nodejs_18 # current LTS
+    nodePackages.pnpm
     nodePackages.typescript-language-server
+    nodePackages.prettier
+    typescript
   ];
 }
