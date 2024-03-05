b4dd465c9a73a906380a1d2a2979a42ba18759c4
Tom Houlé <tom@tomhoule.com>
2024-03-05 08:54:11 +0100

    hm: set up helix-gpt

--- hm/tom/modules/helix.nix
+++ hm/tom/modules/helix.nix
@@ -18,13 +18,28 @@
       };
     };
     languages = {
+      language-server.gpt = {
+        command = "helix-gpt";
+      };
       language = [
         {
           name = "nix";
           formatter = { command = "nixpkgs-fmt"; };
           auto-format = true;
+          language-servers = [ "nil" "gpt" ];
+        }
+        {
+          name = "rust";
+          language-servers = [ "rust-analyzer" "gpt" ];
         }
       ];
     };
   };
+
+  home.packages = [
+    (pkgs.writeShellScriptBin "helix-gpt" ''
+      source ~/local-secrets
+      HANDLER=copilot ${pkgs.helix-gpt}/bin/helix-gpt
+    '')
+  ];
 }
