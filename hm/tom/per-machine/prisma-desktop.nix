b8c387251192e67780c1329389b8662d2df47008
Tom Houlé <tom@tomhoule.com>
2024-04-18 17:13:07 +0200

    prisma-desktop: tweak size

--- hm/tom/per-machine/prisma-desktop.nix
+++ hm/tom/per-machine/prisma-desktop.nix
@@ -5,6 +5,10 @@
     b2-bucket = "prisma-desktop";
     swayInnerGapSize = 4;
     termBgAlpha = 0.95;
+    termFontSize = 13.0;
+    swayExtraConfig = ''
+      output * scale 1.1
+    '';
   };
 
   home.packages = with pkgs; [ postgresql mariadb ]; # for the clients
