df3859e09a30e19a945f0d75a2661a17c3da8080
Tom Houlé <tom@tomhoule.com>
2024-05-04 11:20:14 +0200

    b2: change deprecated arg name

--- hm/tom/modules/b2.nix
+++ hm/tom/modules/b2.nix
@@ -6,26 +6,26 @@ let
     set -x
 
     ${backblaze-b2}/bin/backblaze-b2 sync \
-      --excludeDirRegex '^tom/.cache$' \
-      --excludeDirRegex '^tom/.config/' \
-      --excludeDirRegex '^tom/.mozilla$' \
-      --excludeDirRegex '^tom/.dropbox$' \
-      --excludeDirRegex '^tom/.grafbase$' \
-      --excludeDirRegex '^tom/.local/share$' \
-      --excludeDirRegex '^tom/.local/state/nvim$' \
-      --excludeDirRegex '^tom/.steam$' \
-      --excludeDirRegex '^tom/.vscode-oss$' \
-      --excludeDirRegex '^tom/src/gh$' \
-      --excludeDirRegex '^tom/src/.*/target/debug$' \
-      --excludeDirRegex '^tom/src/.*/target/release$' \
-      --excludeDirRegex '^tom/src/.*/target/doc$' \
-      --excludeDirRegex '^tom/src/.*/zig-cache$' \
-      --excludeDirRegex '^tom/src/.*/node_modules$' \
-      --excludeDirRegex '^tom/src/.*/lake-packages$' \
-      --excludeDirRegex '^tom/src/.*/.direnv$' \
-      --excludeDirRegex '^tom/tmp$' \
-      --excludeAllSymlinks \
-      --keepDays 30 \
+      --exclude-dir-regex '^tom/.cache$' \
+      --exclude-dir-regex '^tom/.config/' \
+      --exclude-dir-regex '^tom/.mozilla$' \
+      --exclude-dir-regex '^tom/.dropbox$' \
+      --exclude-dir-regex '^tom/.grafbase$' \
+      --exclude-dir-regex '^tom/.local/share$' \
+      --exclude-dir-regex '^tom/.local/state/nvim$' \
+      --exclude-dir-regex '^tom/.steam$' \
+      --exclude-dir-regex '^tom/.vscode-oss$' \
+      --exclude-dir-regex '^tom/src/gh$' \
+      --exclude-dir-regex '^tom/src/.*/target/debug$' \
+      --exclude-dir-regex '^tom/src/.*/target/release$' \
+      --exclude-dir-regex '^tom/src/.*/target/doc$' \
+      --exclude-dir-regex '^tom/src/.*/zig-cache$' \
+      --exclude-dir-regex '^tom/src/.*/node_modules$' \
+      --exclude-dir-regex '^tom/src/.*/lake-packages$' \
+      --exclude-dir-regex '^tom/src/.*/.direnv$' \
+      --exclude-dir-regex '^tom/tmp$' \
+      --exclude-all-symlinks \
+      --keep-days 30 \
       /home \
       b2://${config.localHome.b2-bucket}
   '';
