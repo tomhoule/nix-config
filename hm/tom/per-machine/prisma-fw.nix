31a0a343394624adf493439d06387bbf0c846586
Tom Houlé <tom@tomhoule.com>
2024-06-20 14:15:20 +0200

    hm: add kanshi

--- hm/tom/per-machine/prisma-fw.nix
+++ hm/tom/per-machine/prisma-fw.nix
@@ -9,6 +9,38 @@
     termBgAlpha = 0.90;
   };
 
-  home.packages = with pkgs; [ newsboat postgresql ];
+  home.packages = with pkgs; [ newsboat postgresql kanshi ];
+
+  services.kanshi = {
+    enable = true;
+    settings = [
+      {
+        profile = {
+          name = "undocked";
+          outputs = [
+            {
+              criteria = "eDP-1";
+              status = "enable";
+            }
+          ];
+        };
+      }
+      {
+        profile = {
+          name = "docked";
+          outputs = [
+            {
+              criteria = "eDP-1";
+              status = "disable";
+            }
+            {
+              criteria = "DP-3";
+              status = "enable";
+            }
+          ];
+        };
+      }
+    ];
+  };
 }
 
