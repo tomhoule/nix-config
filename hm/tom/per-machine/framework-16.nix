a71d44c90c43c5efb00baee86b100396b5999de6
Tom Houlé <tom@tomhoule.com>
2024-11-22 17:03:16 +0100

    framework16: enable flakes

--- hm/tom/per-machine/framework-16.nix
+++ hm/tom/per-machine/framework-16.nix
@@ -8,4 +8,11 @@
   xdg.enable = false;
   xdg.mimeApps.enable = false;
   xdg.userDirs.enable = false;
+
+  # Only necessary as long as this machine isn't on nixos
+  nix = {
+    package = pkgs.nix;
+    settings.experimental-features = [ "nix-command" "flakes" ];
+  };
+
 }
