2179d2229b749f8d65bbdf8e5f264f33fae0e8f0
Tom Houlé <tom@tomhoule.com>
2024-11-30 10:35:13 +0100

    framework13: remove plasma6

--- flake.nix
+++ flake.nix
@@ -43,7 +43,6 @@
           nixos-cosmic.nixosModules.default
           {
             services.desktopManager.cosmic.enable = true;
-            services.desktopManager.plasma6.enable = true;
             services.displayManager.cosmic-greeter.enable = true;
           }
         ];
