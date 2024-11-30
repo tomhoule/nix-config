7e179901db6bc72df6577e29280d3ecc3ec4db69
Tom Houlé <tom@tomhoule.com>
2024-11-30 10:37:49 +0100

    hm/jj: use the home manager module for jj

--- hm/tom/modules/jj.nix
+++ hm/tom/modules/jj.nix
@@ -1,17 +1,15 @@
 {
-  pkgs,
   userFullName,
   userEmail,
   githubHandle,
   ...
 }: {
-  home.packages = [
-    pkgs.jujutsu
-  ];
-
-  xdg.configFile."jj/config.toml".text = ''
-    user.name = "${userFullName}"
-    user.email = "${userEmail}"
-    git.push-bookmark-prefix = "${githubHandle}-"
-  '';
+  programs.jujutsu = {
+    enable = true;
+    settings = {
+      user.name = userFullName;
+      user.email = userEmail;
+      git.push-bookmark-prefix = "${githubHandle}-";
+    };
+  };
 }
